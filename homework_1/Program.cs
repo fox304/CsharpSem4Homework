798aeab196344f3c11f94af81d67a4c091a52e96
Maks <nordmax69@gmail.com>
2022-08-30 11:48:14 +0700

    сделал 1 задание

---
+++ homework_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// В одномерном массиве из 123 чисел найти кол-во элементов из отрезка [10,99]
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Зададим массив из 123 чисел ");
+int [] array = new int [123];
+void FillPrintArray(int [] arr)
+{
+    for (int i = 0;i<arr.Length;i++)
+    {
+        arr[i]=new Random().Next(1,200);
+        Console.Write($"{arr[i]} ");
+    }
+}
+int CountElementsInTheRange(int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]>9 && arr[i]<100) count++;
+    }
+    return count;
+}
+FillPrintArray(array);
+Console.WriteLine();
+int res = CountElementsInTheRange(array);
+Console.WriteLine($"Кол-во элементов в массиве из отрезка [10,99] равно {res}");
